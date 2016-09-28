3e77119017f10e10f250ffa31daadd53f164ab78
Carlos C <uldericofilho@gmail.com>
2016-09-29 01:04:40 +0200

    supervisor tree for Go applications

---
+++ supervisor.go
@@ -0,0 +1,284 @@
+/*
+Package supervisor provides supervisor trees for Go applications.
+
+This package is a clean reimplementation of github.com/thejerf/suture, aiming
+to be more Go idiomatic, thus less Erlang-like.
+
+It is built on top of context package, with all of its advantages, namely the
+possibility trickle down context-related values and cancellation signals.
+
+TheJerf's blog post about Suture is a very good and helpful read to understand
+how this package has been implemented.
+
+http://www.jerf.org/iri/post/2930
+*/
+package supervisor // import "cirello.io/supervisor"
+
+import (
+	"fmt"
+	"log"
+	"math"
+	"sync"
+	"time"
+
+	"golang.org/x/net/context"
+)
+
+// Service is the public interface expected by a Supervisor.
+//
+// This will be internally named after the result of fmt.Stringer, if available.
+// Otherwise it will going to use an internal representation for the service
+// name.
+type Service interface {
+	// Serve is called by a Supervisor to start the service. It expects the
+	// service to honor the passed context and its lifetime. Observe
+	// <-ctx.Done() and ctx.Err(). If the service is stopped by anything
+	// but the Supervisor, it will get started again. Be careful with shared
+	// state among restarts.
+	Serve(ctx context.Context)
+}
+
+// Supervisor is the basic datastructure responsible for offering a supervisor
+// tree. It implements Service, therefore it can be nested if necessary. When
+// passing the Supervisor around, remind to do it as reference (&supervisor).
+type Supervisor struct {
+
+	// Name for this supervisor tree, used for logging.
+	Name string
+
+	// FailureDecay is the timespan on which the current failure count will
+	// be halved.
+	FailureDecay float64
+
+	// FailureThreshold is the maximum accepted number of failures, after
+	// decay adjustment, that shall trigger the back-off wait.
+	FailureThreshold float64
+
+	// Backoff is the wait duration when hit threshold.
+	Backoff time.Duration
+
+	// Log is a replaceable function used for overall logging
+	Log func(string)
+
+	ready     sync.Once
+	startOnce sync.Once
+
+	addedService    chan struct{}
+	startedServices chan struct{}
+	stoppedService  chan struct{}
+
+	servicesMu sync.Mutex
+	services   map[string]Service
+
+	cancellationsMu sync.Mutex
+	cancellations   map[string]context.CancelFunc
+
+	backoffMu sync.Mutex
+	backoff   map[string]*backoff
+}
+
+func (s *Supervisor) String() string {
+	return s.Name
+}
+
+func (s *Supervisor) prepare() {
+	s.ready.Do(func() {
+		if s.Name == "" {
+			s.Name = "supervisor"
+		}
+		s.addedService = make(chan struct{}, 1)
+		s.backoff = make(map[string]*backoff)
+		s.cancellations = make(map[string]context.CancelFunc)
+		s.services = make(map[string]Service)
+		s.startedServices = make(chan struct{})
+		s.stoppedService = make(chan struct{}, 1)
+
+		if s.Log == nil {
+			s.Log = func(str string) {
+				log.Println(s.Name, ":", str)
+			}
+		}
+		if s.FailureDecay == 0 {
+			s.FailureDecay = 30
+		}
+		if s.FailureThreshold == 0 {
+			s.FailureThreshold = 5
+		}
+		if s.Backoff == 0 {
+			s.Backoff = 15 * time.Second
+		}
+	})
+}
+
+// Add inserts into the Supervisor tree a new service. If the Supervisor is
+// already started, it will start it automatically.
+func (s *Supervisor) Add(service Service) {
+	s.prepare()
+
+	name := fmt.Sprintf("%s", service)
+
+	s.servicesMu.Lock()
+	s.backoffMu.Lock()
+	s.backoff[name] = &backoff{}
+	s.services[name] = service
+	s.backoffMu.Unlock()
+	s.servicesMu.Unlock()
+
+	select {
+	case s.addedService <- struct{}{}:
+	default:
+	}
+}
+
+// Remove stops the service in the Supervisor tree and remove from it.
+func (s *Supervisor) Remove(name string) {
+	s.prepare()
+
+	s.servicesMu.Lock()
+	defer s.servicesMu.Unlock()
+
+	if _, ok := s.services[name]; !ok {
+		return
+	}
+
+	s.cancellationsMu.Lock()
+	defer s.cancellationsMu.Unlock()
+	if c, ok := s.cancellations[name]; ok {
+		delete(s.cancellations, name)
+		c()
+	}
+}
+
+// Serve starts the Supervisor tree. It can be started only once.
+func (s *Supervisor) Serve(ctx context.Context) {
+	s.prepare()
+	s.startOnce.Do(func() {
+		s.serve(ctx)
+	})
+}
+
+// Services return a list of service names and their cancellation calls
+func (s *Supervisor) Services() map[string]context.CancelFunc {
+	svclist := make(map[string]context.CancelFunc)
+	s.cancellationsMu.Lock()
+	for k, v := range s.cancellations {
+		svclist[k] = v
+	}
+	s.cancellationsMu.Unlock()
+	return svclist
+}
+
+func (s *Supervisor) serve(ctx context.Context) {
+	go func(ctx context.Context) {
+		for range s.addedService {
+			s.startServices(ctx)
+			select {
+			case s.startedServices <- struct{}{}:
+			default:
+			}
+		}
+	}(ctx)
+
+	<-ctx.Done()
+	s.cancellationsMu.Lock()
+	for _, c := range s.cancellations {
+		c()
+	}
+	s.cancellations = make(map[string]context.CancelFunc)
+	s.cancellationsMu.Unlock()
+
+	for range s.stoppedService {
+		s.servicesMu.Lock()
+		l := len(s.services)
+		s.servicesMu.Unlock()
+		if l == 0 {
+			return
+		}
+	}
+}
+
+func (s *Supervisor) startServices(ctx context.Context) {
+	s.servicesMu.Lock()
+	defer s.servicesMu.Unlock()
+
+	var wg sync.WaitGroup
+
+	for name, svc := range s.services {
+		s.cancellationsMu.Lock()
+		_, ok := s.cancellations[name]
+		s.cancellationsMu.Unlock()
+		if ok {
+			continue
+		}
+
+		wg.Add(1)
+
+		go func(name string, svc Service) {
+			wg.Done()
+			for {
+				retry := func() (retry bool) {
+					select {
+					case <-ctx.Done():
+						return false
+					default:
+					}
+
+					defer func() {
+						if r := recover(); r != nil {
+							s.Log(fmt.Sprint("trapped panic:", r))
+							retry = true
+						}
+					}()
+
+					c, cancel := context.WithCancel(ctx)
+					s.cancellationsMu.Lock()
+					s.cancellations[name] = cancel
+					s.cancellationsMu.Unlock()
+					svc.Serve(c)
+
+					select {
+					case <-c.Done():
+						return false
+					default:
+						return true
+					}
+				}()
+				if retry {
+					s.Log(fmt.Sprintf("restarting %s", name))
+					s.backoffMu.Lock()
+					b := s.backoff[name]
+					s.backoffMu.Unlock()
+					b.wait(s.FailureDecay, s.FailureThreshold, s.Backoff)
+					continue
+				}
+
+				s.servicesMu.Lock()
+				delete(s.services, name)
+				s.servicesMu.Unlock()
+				s.stoppedService <- struct{}{}
+				return
+			}
+		}(name, svc)
+	}
+	wg.Wait()
+}
+
+type backoff struct {
+	lastfail time.Time
+	failures float64
+}
+
+func (b *backoff) wait(failureDecay float64, threshold float64, backoffDur time.Duration) {
+	if b.lastfail.IsZero() {
+		b.lastfail = time.Now()
+		b.failures = 1.0
+	} else {
+		b.failures++
+		intervals := time.Since(b.lastfail).Seconds() / failureDecay
+		b.failures = b.failures*math.Pow(.5, intervals) + 1
+	}
+
+	if b.failures > threshold {
+		time.Sleep(backoffDur)
+	}
+}
---
+++ supervisor_test.go
@@ -0,0 +1,273 @@
+package supervisor
+
+import (
+	"fmt"
+	"testing"
+	"time"
+
+	"golang.org/x/net/context"
+)
+
+type simpleservice int
+
+func (s *simpleservice) String() string {
+	return fmt.Sprintf("simple service %d", int(*s))
+}
+
+func (s *simpleservice) Serve(ctx context.Context) {
+	var i int
+	for {
+		i++
+		fmt.Println("service started:", *s, "iteration:", i)
+		select {
+		case <-ctx.Done():
+			fmt.Println("context done:", ctx.Err(), *s)
+			return
+		default:
+			time.Sleep(500 * time.Millisecond)
+		}
+	}
+}
+
+func ExampleSupervisor() {
+	var supervisor Supervisor
+
+	svc := simpleservice(1)
+	supervisor.Add(&svc)
+
+	ctx, _ := context.WithTimeout(context.Background(), 1*time.Second)
+	supervisor.Serve(ctx)
+}
+
+func TestString(t *testing.T) {
+	const expected = "test"
+	var supervisor Supervisor
+	supervisor.Name = expected
+
+	if got := fmt.Sprintf("%s", &supervisor); got != expected {
+		t.Errorf("error getting supervisor name: %s", got)
+	}
+}
+
+func TestSimple(t *testing.T) {
+	var supervisor Supervisor
+
+	svc := simpleservice(1)
+	supervisor.Add(&svc)
+
+	ctx, _ := context.WithTimeout(context.Background(), 1*time.Second)
+	supervisor.Serve(ctx)
+
+	countService(t, &supervisor)
+}
+
+func TestMultiple(t *testing.T) {
+	var supervisor Supervisor
+
+	svc1 := simpleservice(2)
+	supervisor.Add(&svc1)
+	svc2 := simpleservice(3)
+	supervisor.Add(&svc2)
+
+	ctx, _ := context.WithTimeout(context.Background(), 1*time.Second)
+	supervisor.Serve(ctx)
+
+	countService(t, &supervisor)
+}
+
+func TestCascaded(t *testing.T) {
+	var supervisor Supervisor
+
+	svc1 := simpleservice(4)
+	supervisor.Add(&svc1)
+	svc2 := simpleservice(5)
+	supervisor.Add(&svc2)
+
+	var childSupervisor Supervisor
+	svc3 := simpleservice(6)
+	childSupervisor.Add(&svc3)
+	svc4 := simpleservice(7)
+	childSupervisor.Add(&svc4)
+
+	supervisor.Add(&childSupervisor)
+
+	ctx, _ := context.WithTimeout(context.Background(), 1*time.Second)
+	supervisor.Serve(ctx)
+
+	countService(t, &supervisor)
+}
+
+type panicservice int
+
+func (s *panicservice) Serve(ctx context.Context) {
+	for {
+		fmt.Println("panic service started:", *s)
+		select {
+		case <-ctx.Done():
+			fmt.Println("panic service context:", ctx.Err(), *s)
+			return
+		default:
+			time.Sleep(100 * time.Millisecond)
+			panic("forcing panic")
+		}
+	}
+}
+
+func TestPanic(t *testing.T) {
+	var supervisor Supervisor
+
+	svc1 := panicservice(1)
+	supervisor.Add(&svc1)
+
+	ctx, _ := context.WithTimeout(context.Background(), 1*time.Second)
+	supervisor.Serve(ctx)
+
+	countService(t, &supervisor)
+}
+
+type failingservice int
+
+func (s *failingservice) Serve(ctx context.Context) {
+	for {
+		fmt.Println("failing service started:", *s, "times")
+		select {
+		case <-ctx.Done():
+			fmt.Println("failing service context:", ctx.Err(), *s, "times")
+			return
+		default:
+			time.Sleep(100 * time.Millisecond)
+			*s++
+			return
+		}
+	}
+}
+
+func (s *failingservice) String() string {
+	return fmt.Sprintf("failing service %v", *s)
+}
+
+func TestFailing(t *testing.T) {
+	supervisor := Supervisor{
+		Backoff: 1 * time.Second,
+		Log: func(msg string) {
+			t.Log("supervisor log:", msg)
+		},
+	}
+
+	svc1 := failingservice(1)
+	supervisor.Add(&svc1)
+
+	ctx, _ := context.WithTimeout(context.Background(), 3*time.Second)
+	supervisor.Serve(ctx)
+
+	countService(t, &supervisor)
+}
+
+func TestAddServiceAfterServe(t *testing.T) {
+	var supervisor Supervisor
+
+	svc1 := simpleservice(1)
+	supervisor.Add(&svc1)
+
+	ctx, cancel := context.WithTimeout(context.Background(), 2*time.Second)
+	done := make(chan struct{})
+	go func() {
+		supervisor.Serve(ctx)
+		done <- struct{}{}
+	}()
+
+	<-supervisor.startedServices
+	svc2 := simpleservice(2)
+	supervisor.Add(&svc2)
+	<-supervisor.startedServices
+
+	cancel()
+	<-ctx.Done()
+	<-done
+
+	countService(t, &supervisor)
+}
+
+func TestRemoveServiceAfterServe(t *testing.T) {
+	var supervisor Supervisor
+
+	svc1 := simpleservice(1)
+	supervisor.Add(&svc1)
+	svc2 := simpleservice(2)
+	supervisor.Add(&svc2)
+
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	done := make(chan struct{})
+
+	go func() {
+		supervisor.Serve(ctx)
+		done <- struct{}{}
+	}()
+
+	lbefore := getServiceCount(&supervisor)
+	supervisor.Remove("unknown service")
+	lafter := getServiceCount(&supervisor)
+
+	if lbefore != lafter {
+		t.Error("the removal of an unknown service shouldn't happen")
+	}
+
+	<-supervisor.startedServices
+	supervisor.Remove(svc1.String())
+	fmt.Println("removed service")
+
+	lremoved := getServiceCount(&supervisor)
+	if lbefore != lremoved {
+		t.Error("the removal of a service should have affected the supervisor:", lbefore, lremoved)
+	}
+
+	cancel()
+	<-ctx.Done()
+	<-done
+
+	countService(t, &supervisor)
+}
+
+func countService(t *testing.T, supervisor *Supervisor) {
+	if l := len(supervisor.services); l != 0 {
+		t.Errorf("not all services were stopped. possibly a bug: %d services left", l)
+	}
+}
+
+func getServiceCount(s *Supervisor) int {
+	s.servicesMu.Lock()
+	l := len(s.services)
+	s.servicesMu.Unlock()
+	return l
+}
+
+func TestServices(t *testing.T) {
+	var supervisor Supervisor
+
+	svc1 := simpleservice(1)
+	supervisor.Add(&svc1)
+	svc2 := simpleservice(2)
+	supervisor.Add(&svc2)
+
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	done := make(chan struct{})
+
+	go func() {
+		supervisor.Serve(ctx)
+		done <- struct{}{}
+	}()
+
+	<-supervisor.startedServices
+	svcs := supervisor.Services()
+	fmt.Println(svcs)
+	for _, svcname := range []string{svc1.String(), svc2.String()} {
+		if _, ok := svcs[svcname]; !ok {
+			t.Errorf("expected service not found: %s", svcname)
+		}
+	}
+
+	cancel()
+	<-ctx.Done()
+	<-done
+
+}

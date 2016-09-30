5358fdc938f4339581619c8facae48c176fdcc9b
Carlos C <uldericofilho@gmail.com>
2016-09-30 23:24:08 +0200

    Add package-level supervisor to facilitate use.
    Add a package-level thus simplifying the package use. No need to
    declare supervisor to variable, the package holds all the necessary
    magic. Because, DefaultSupervisor is public, it also means it is
    adjustable.

---
+++ stdsupervisor.go
@@ -0,0 +1,49 @@
+package supervisor
+
+import "golang.org/x/net/context"
+
+// DefaultSupervisor is the default Supervisor used in this package.
+var DefaultSupervisor Supervisor
+var defaultContext context.Context
+
+func init() {
+	DefaultSupervisor.Name = "default supervisor"
+	defaultContext = context.Background()
+}
+
+// Add inserts new service into the DefaultSupervisor. If the DefaultSupervisor
+// is already started, it will start it automatically.
+func Add(service Service) {
+	DefaultSupervisor.Add(service)
+}
+
+// Remove stops the service in the DefaultSupervisor tree and remove from it.
+func Remove(name string) {
+	DefaultSupervisor.Remove(name)
+}
+
+// Services return a list of services of DefaultSupervisor
+func Services() map[string]Service {
+	return DefaultSupervisor.Services()
+}
+
+// Cancelations return a list of services names of DefaultSupervisor and their
+// cancellation calls.
+func Cancelations() map[string]context.CancelFunc {
+	return DefaultSupervisor.Cancelations()
+}
+
+// Serve starts the DefaultSupervisor tree. It can be started only once at a
+// time. If stopped (canceled), it can be restarted. In case of concurrent
+// calls, it will hang until the current call is completed.
+func Serve() {
+	ServeContext(defaultContext)
+}
+
+// ServeContext starts the DefaultSupervisor tree with a custom context.Context.
+// It can be started only once at a time. If stopped (canceled), it can be
+// restarted. In case of concurrent calls, it will hang until the current call
+// is completed.
+func ServeContext(ctx context.Context) {
+	DefaultSupervisor.Serve(ctx)
+}

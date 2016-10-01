ba54c15ecaabf633ffcb2294739283aeb157b383
Carlos C <uldericofilho@gmail.com>
2016-10-01 20:47:10 +0200

    Merge two global tests

--- stdsupervisor_test.go
+++ stdsupervisor_test.go
@@ -20,7 +20,7 @@ func ExampleServeGroup() {
 	ServeGroup()
 }
 
-func TestDefaultSupevisor(t *testing.T) {
+func TestDefaultSupevisorAndGroup(t *testing.T) {
 	t.Parallel()
 
 	ctx, cancel := contextWithTimeout(10 * time.Second)
@@ -61,47 +61,17 @@ func TestDefaultSupevisor(t *testing.T) {
 
 	cancel()
 	wg.Wait()
-}
 
-func TestServeGroup(t *testing.T) {
-	t.Parallel()
-
-	ctx, cancel := contextWithTimeout(10 * time.Second)
+	ctx, cancel = contextWithTimeout(10 * time.Second)
 	defaultContext = ctx
-	svc := &holdingservice{id: 1}
-	svc.Add(1)
-
-	Add(svc)
-	if len(DefaultSupervisor.services) != 1 {
-		t.Errorf("%s should have been added", svc.String())
-	}
-
-	Remove(svc.String())
-	if len(DefaultSupervisor.services) != 0 {
-		t.Errorf("%s should have been removed. services: %#v", svc.String(), DefaultSupervisor.services)
-	}
-
-	Add(svc)
-
-	svcs := Services()
-	if _, ok := svcs[svc.String()]; !ok {
-		t.Errorf("%s should have been found", svc.String())
-	}
-
-	var wg sync.WaitGroup
 	wg.Add(1)
+	svc.Add(1)
 	go func() {
 		ServeGroup()
 		wg.Done()
 	}()
 
 	svc.Wait()
-
-	cs := Cancelations()
-	if _, ok := cs[svc.String()]; !ok {
-		t.Errorf("%s's cancelation should have been found", svc.String())
-	}
-
 	cancel()
 	wg.Wait()
 }

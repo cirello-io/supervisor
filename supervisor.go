96413cc4af4da688f1d99ed7bbd1f9d3d60f1fdb
Carlos C <uldericofilho@gmail.com>
2016-10-03 07:56:08 +0200

    Add support for permanent, transient and temporary services

--- supervisor.go
+++ supervisor.go
@@ -5,23 +5,51 @@ import "fmt"
 // AlwaysRestart adjusts the supervisor to never halt in face of failures.
 const AlwaysRestart = -1
 
+// ServiceType defines the restart strategy for a service.
+type ServiceType int
+
+const (
+	// Permanent services are always restarted
+	Permanent ServiceType = iota
+	// Transient services are restarted only when panic.
+	Transient
+	// Temporary services are never restarted.
+	Temporary
+)
+
+type service struct {
+	svc     Service
+	svctype ServiceType
+}
+
 type processFailure func()
 
 func (s *Supervisor) String() string {
 	return s.Name
 }
 
-// Add inserts into the Supervisor tree a new service. If the Supervisor is
-// already started, it will start it automatically. If the same service is added
-// more than once, it will reset its backoff mechanism and force a service
-// restart.
+// Add inserts into the Supervisor tree a new permanent service. If the
+// Supervisor is already started, it will start it automatically. If the same
+// service is added more than once, it will reset its backoff mechanism and
+// force a service restart.
 func (s *Supervisor) Add(service Service) {
+	s.AddService(service, Permanent)
+}
+
+// AddService inserts into the Supervisor tree a new service of ServiceType. If
+// the Supervisor is already started, it will start it automatically. If the
+// same service is added more than once, it will reset its backoff mechanism and
+// force a service restart.
+func (s *Supervisor) AddService(svc Service, svctype ServiceType) {
 	s.prepare()
 
-	name := fmt.Sprintf("%s", service)
+	name := fmt.Sprintf("%s", svc)
 
 	s.mu.Lock()
-	s.services[name] = service
+	s.services[name] = service{
+		svc:     svc,
+		svctype: svctype,
+	}
 	s.mu.Unlock()
 
 	select {
@@ -57,7 +85,7 @@ func (s *Supervisor) Services() map[string]Service {
 	svclist := make(map[string]Service)
 	s.mu.Lock()
 	for k, v := range s.services {
-		svclist[k] = v
+		svclist[k] = v.svc
 	}
 	s.mu.Unlock()
 	return svclist
